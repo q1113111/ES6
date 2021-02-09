72259ddb79f8b027935acc670c9d34cbf099cb55
Lai Sam <q1113111@gmail.com>
2021-02-09 17:20:56 +0800

    2021/02/09 eslint 更新與技術

--- test.js
+++ test.js
@@ -1,63 +1,30 @@
-const repos = [
-   {
-      name: 'grit',
-      owner: 'mojombo',
-      description: 'Grit is no longer',
-      id: 1,
-   },
-   {
-      name: 'jsawesome',
-      owner: 'sam',
-      description: 'go to javascript',
-      id: 2,
-   },
-   {
-      name: 'htmlcss',
-      owner: 'web',
-      description: 'html add css is good',
-      id: 3,
-   },
-];
-const owers = [
-   {
-      name: 'mojombo',
-      location: 'Taiwan',
-      followers: 123,
-   },
-   {
-      name: 'sam',
-      location: 'USA',
-      followers: 1523,
-   },
-   {
-      name: 'web',
-      location: 'Japanes',
-      followers: 423,
-   },
-];
+class Animal {
+    constructor(name) {
+      this.name = name;
+      this.belly = [];
+    }
 
-function getrepseId(id) {
-   return new Promise((reslove, reject) => {
-      const repo = repos.find((repo) => repo.id === id);
-      repo ? reslove(repo) : reject(Error('no repo found'));
-   });
-}
+    eat(food) {
+      this.belly.push(food);
+    }
+  }
+  //es5寫法---
+  function Dog(name,age){
+    Animal.call(this,name,age);
+    this.name = name;
+    this.age = age
+  }
+  Dog.prototype = new Animal();
+  Dog.prototype.consturctor = Dog;
+  //---
+  class Dog extends Animal {
+    constructor(name, age) {
+      super(name);
+      this.age = age;
+    }
 
-function comboundOwner(repo) {
-   return new Promise((reslove, reject) => {
-      const owner = owers.find((owner) => owner.name === repo.owner);
-      repo.owner = owner;
-      repo.owner ? reslove(repo) : reject(Error('Can not find owner'));
-   });
-}
-
-getrepseId(1)
-   .then((repo) => {
-      return comboundOwner(repo);
-   })
-   .then((repo) => {
-      console.log(repo);
-   })
-   .catch((error) => {
-      console.error(error);
-   });
+    bark() {
+      console.log(`Bark bark`);
+    }
+  }
+  const lucky = new Dog("lucky", 2);
